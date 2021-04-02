b8abd0a18f9148b30080c6394b30cc4f73ae5e19
Rick <1447076355@qq.com>
2021-04-02 10:45:20 +0800

    初始化

--- src/test/java/com/rick/WarehousingApplicationTests.java
+++ src/test/java/com/rick/WarehousingApplicationTests.java
@@ -1,14 +1,35 @@
 package com.rick;
 
+import com.rick.sys.entity.SysUser;
+import com.rick.sys.mapper.SysUserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class WarehousingApplicationTests {
 
+    @Resource
+    private SysUserMapper sysUserMapper;
+
     @Test
     void contextLoads() {
-        System.out.println("success");
+        Map<String, Object> map = new HashMap<>();
+        map.put("address","武汉");
+        List<SysUser> sysUsers = this.sysUserMapper.selectByMap(map);
+        print(sysUsers);
+
+    }
+
+    public void print(List<SysUser> sysUsers){
+        for (SysUser user: sysUsers
+             ) {
+            System.out.println(user);
+        }
     }
 
 }
