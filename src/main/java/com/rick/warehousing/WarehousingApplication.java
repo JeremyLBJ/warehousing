fdc2c991e9f97f8808f3130f60364707d3d24b83
Rick <1447076355@qq.com>
2021-04-02 09:44:23 +0800

    初始化

---
+++ src/main/java/com/rick/warehousing/WarehousingApplication.java
@@ -0,0 +1,13 @@
+package com.rick.warehousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehousingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehousingApplication.class, args);
+    }
+
+}
