9252d110909d5f8bc063731b0101fc0c775931ea
Rick <1447076355@qq.com>
2021-04-02 10:17:07 +0800

    初始化

--- src/main/java/com/rick/WarehousingApplication.java
+++ src/main/java/com/rick/WarehousingApplication.java
@@ -1,9 +1,11 @@
 package com.rick;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.rick.sys.mapper"})
 public class WarehousingApplication {
 
     public static void main(String[] args) {
