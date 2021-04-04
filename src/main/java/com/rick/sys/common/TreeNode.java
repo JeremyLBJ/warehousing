cf47fc0ddcfd3ffc98a697e2c98ae35323937faa
Rick <1447076355@qq.com>
2021-04-04 22:37:05 +0800

    修改菜单

---
+++ src/main/java/com/rick/sys/common/TreeNode.java
@@ -0,0 +1,40 @@
+package com.rick.sys.common;
+
+import com.rick.sys.entity.SysPermission;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Rick
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class TreeNode {
+
+    private Integer id;
+    private Integer pid;
+    private String title;
+    private String icon;
+    private String href;
+    private Boolean spread;
+    private List<TreeNode> children=new ArrayList<>();
+    /**
+     *首页左边导航树的构造器
+     */
+    public TreeNode(Integer id, Integer pid, String title, String icon, String href, Boolean spread) {
+        super();
+        this.id = id;
+        this.pid = pid;
+        this.title = title;
+        this.icon = icon;
+        this.href = href;
+        this.spread = spread;
+    }
+}
