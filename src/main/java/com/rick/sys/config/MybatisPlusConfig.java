82a16a5a1f5816a2476390045321a3c0b7f4858d
Rick <1447076355@qq.com>
2021-04-07 16:07:14 +0800

    initial_LogInfo

--- src/main/java/com/rick/sys/config/MybatisPlusConfig.java
+++ src/main/java/com/rick/sys/config/MybatisPlusConfig.java
@@ -1,15 +1,15 @@
 package com.rick.sys.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @Ahthro Rick
  * MybatisPlus 分页插件配置类
  */
-@Configurable
+@Configuration
 @ConditionalOnClass(value = PaginationInterceptor.class)
 public class MybatisPlusConfig {
 
